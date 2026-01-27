32a45d8778676c9660f03beb99844a838dbb43c5
skachhi900-lgtm <skachhi900@gmail.com>
2026-01-27 14:08:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function calculatePercentage() {
   document.getElementById("result").innerText =
     percent + "% of " + value + " = " + result;
 }
+function calculateGST() {
+  let price = document.getElementById("gstPrice").value;
+  let gst = document.getElementById("gstPercent").value;
+
+  if(price === "" || gst === "") {
+    document.getElementById("gstResult").innerText = "Please enter both values";
+    return;
+  }
+
+  let gstAmount = (price * gst) / 100;
+  let total = parseFloat(price) + parseFloat(gstAmount);
+
+  document.getElementById("gstResult").innerText =
+    "GST: " + gstAmount + " | Total Price: " + total
+    ;
+}
