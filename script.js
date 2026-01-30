1e044a4cb16bc2ec13661db6483bd01657c6cf3c
calckgrow <skachhi900@gmail.com>
2026-01-30 11:59:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,30 +17,7 @@ function calculateInsta() {
 }
 
 /* --- FINANCE --- */
-function calculateEMI() {
-  let P = document.getElementById("loan").value;
-  let R = document.getElementById("rate").value / 12 / 100;
-  let N = document.getElementById("time").value * 12;
 
-  if (P === "" || R === 0 || N === 0) {
-    alert("Please enter valid values");
-    return;
-  }
-
-  let EMI = (P * R * Math.pow(1 + R, N)) / (Math.pow(1 + R, N) - 1);
-
-  let totalPayment = EMI * N;
-  let totalInterest = totalPayment - P;
-
-  document.getElementById("emi").innerText =
-    "Monthly EMI: ₹" + EMI.toFixed(2);
-
-  document.getElementById("interest").innerText =
-    "Total Interest: ₹" + totalInterest.toFixed(2);
-
-  document.getElementById("total").innerText =
-    "Total Payment: ₹" + totalPayment.toFixed(2);
-}
 
 function calculateSIP() {
     const p = document.getElementById('sipAmount').value;
